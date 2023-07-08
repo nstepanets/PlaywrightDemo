493b0658312e45a717733f863cd629788f9cdcaf
Mykola Stepanets <nik.stepanets@gmail.com>
2023-07-09 02:29:08 +0300

    Added test with using Playwright.NUnit

---
+++ PlaywrightDemo/NUnitPlaywright.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Playwright.NUnit;
+
+namespace PlaywrightDemo
+{
+	public class NUnitPlaywright : PageTest
+	{
+		[SetUp]
+		public async Task Setup()
+		{
+            await Page.GotoAsync("https://playwright.dev/dotnet");
+        }
+
+		[Test]
+		public async Task NUnitTest()
+		{
+			await Page.ClickAsync("xpath=//a[contains(@class, 'getStarted')]");
+			await Page.ClickAsync("xpath=//h1[text()='Installation']");
+
+			await Expect(Page.Locator("xpath=//h1[text()='Installation']")).ToBeVisibleAsync();
+		}
+	}
+}
+
